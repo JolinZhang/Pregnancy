730a86d38d4e2ca90dd21ab23288a374c8964c39
Ru Zhang <zhangru0026@gmail.com>
2020-08-11 22:15:29 -0500

    First commit

---
+++ src/app/foodlog/foodlog.component.ts
@@ -0,0 +1,11 @@
+
+import {Component} from '@angular/core';
+
+@Component({
+selector: 'app-foodlog',
+templateUrl: './foodlog.component.html'
+})
+
+export class FoodlogComponent{
+    title = 'food log'
+}
